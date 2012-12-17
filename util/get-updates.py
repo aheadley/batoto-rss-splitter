ae746e3714c5ab0e7b7aa21f38f04663cc86a74e
aheadley <aheadley@waysaboutstuff.com>
2012-12-17 00:45:47 -0500

    initial commit

---
+++ util/get-updates.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import requests
+import feedparser
+import re
+
+class Updater(object):
+    """
+    """
+
+    PATTERN_DESC = re.compile(
+        r'^(?P<series>.*) - (?P<lang>[A-Z][a-z]+) - '
+            '(?P<chapter>(?:Vol\.[\d.]+ )?Ch\.[\d.]+):? '
+            '(?P<chapter_title>.*)$')
+
+    def __init__(self, feed_url):
+        self._feed_url = feed_url
+        self._feed = None
+
+    def update(self):
+        for entry in self._get_feed().entries:
+            print self.PATTERN_DESC.match(entry.title).groupdict()
+
+    def _get_feed(self):
+        if self._feed is None:
+            self._feed = feedparser.parse(self._feed_url)
+        return self._feed
+
+
+if __name__ == '__main__':
+    import os
+    import sys
+
+    feed_url = os.environ.get('BATOTO_FEED_URL',
+        'http://www.batoto.net/recent_rss')
+    try:
+        feed_url = sys.argv[1]
+    except IndexError:
+        pass
+    updater = Updater(feed_url)
+    updater.update()
